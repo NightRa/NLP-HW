d922c698f54c810b89d03e56a4997291ee62eaec
NightRa <ilan3580@gmail.com>
2016-04-11 21:32:11 +0300

    Factor out count_probabilities

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -9,12 +9,14 @@ with codecs.open(file, 'r', 'utf-8') as f:
 
 sentences = re.sub(r"(\r\n)+|\n+|\xa0|\t", "\n", text).split("\n")
 
+# flatMap: [a], (a -> [b]) -> [b]
 def flatMap(list, f):
     list_out = []
     for e in list:
         list_out.extend(f(e))
     return list_out
 
+# returns the sliding windows of size n of the list.
 # window: [A] -> [(a1, a2, ..., an)]
 def window(list, n):
     return [tuple(list[i:i + n]) for i in range(len(list) - n + 1)]
@@ -39,26 +41,32 @@ def get_bigrams(sentences):
 def get_trigrams(sentences):
     return n_grams(sentences, 3)
 
-# count_frequencies: [(String, String)] -> Map (String, String) -> Int
-def count_frequencies(bigrams):
+# count_frequencies: List[A] -> Map[A, Int (#occurences)]
+def count_frequencies(elements):
     # foldMap (Map(_ -> 1))
     counts = {}
-    for bigram in bigrams:
-        if bigram not in counts:
-            counts[bigram] = 1
+    for e in elements:
+        if e not in counts:
+            counts[e] = 1
         else:
-            counts[bigram] += 1
+            counts[e] += 1
     return counts
+
+# problem: we were asked to divide by #unigrams in raw_frequency, not by #bigrams.
+# count_probabilities: List[A] -> Map[A, Double (prob. max likelihood estimation)]
+def count_probabilities(elements, total):
+    frequencies = count_frequencies(elements)
+    def normalize(freq):
+        return freq / total
+    # frequencies.map(normalize)
+    return {k: normalize(v) for k, v in frequencies.items()}
+
 
 def raw_frequency(sentences):
     tokens = get_unigrams(sentences)
     num_tokens = len(tokens)
     bigrams = get_bigrams(sentences)
-    frequencies = count_frequencies(bigrams)
-    def normalize(freq):
-        return (freq / num_tokens) * 1000
-    # frequencies.map(normalize)
-    return {k: normalize(v) for k, v in frequencies.items()}
+    return count_probabilities(bigrams, num_tokens)
 
 # frequencies: Map[(String, String), Double]
 frequencies = raw_frequency(sentences)
@@ -68,4 +76,4 @@ sorted_frequencies = sorted(frequencies.items(), key=(lambda pair: pair[1]), rev
                                                         # ^ freq
 for (bigram, freq) in sorted_frequencies:
   w1, w2 = bigram
-  print(w1, w2, '=', "%.3f" % freq)
+  print(w1, w2, '=', "%.3f" % (freq * 1000))
